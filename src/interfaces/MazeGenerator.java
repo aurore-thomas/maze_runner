984700c7ea34168d0ec6b0450de9c5735b7a1370
aurore-thomas <aurore.thomas@laplateforme.io>
2023-09-25 17:43:26 +0200

    [ADD] Methods in interface

--- src/interfaces/MazeGenerator.java
+++ src/interfaces/MazeGenerator.java
@@ -1,5 +1,7 @@
 package interfaces;
 
 public interface MazeGenerator {
-
+    public void initializeMaze(int lin, int col);
+    public boolean[][] generate(int x, int y);
+    public void print(boolean[][] maze, int lin, int col);
 }
