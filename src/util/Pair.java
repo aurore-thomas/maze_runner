b1bf844333d8c097219169b9475e520c4824007b
aurore-thomas <aurore.thomas@laplateforme.io>
2023-09-27 16:54:47 +0200

    [FIX] Contains method

--- src/util/Pair.java
+++ src/util/Pair.java
@@ -1,9 +1,16 @@
 package util;
 
+import java.util.ArrayList;
+
+/*
+I recreate the Pair class and the contains method which will be used with the unvisitedCells arraylist
+I could also have used the Pair class by injecting a dependency, but I only need one method.
+ */
 public class Pair<T, U> {
     private T first;
     private U second;
 
+    // Getters & setters
     public Pair(T first, U second) {
         this.first = first;
         this.second = second;
@@ -17,7 +24,13 @@ public class Pair<T, U> {
         return second;
     }
 
-    public boolean contains(T firstValue, U secondValue) {
-        return first.equals(firstValue) && second.equals(secondValue);
+
+    public  static <T, U> boolean containsPair(ArrayList<Pair<T, U>> list, Pair<T, U> targetPair) {
+        for (Pair<T, U> pair : list) {
+            if (pair.getFirst().equals(targetPair.getFirst()) && pair.getSecond().equals(targetPair.getSecond())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
