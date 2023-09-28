dfa2191892fb2b0ea1c7f4ffc7ff6d7bf33bbc45
aurore-thomas <aurore.thomas@laplateforme.io>
2023-09-28 14:52:22 +0200

    [DEL] Delete old architecture

--- src/interfaces/MazeGenerator.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-public interface MazeGenerator {
-    public void initializeMaze(int lin, int col);
-    public boolean[][] generateCenterMaze(int x, int y);
-    public void lastLineMaze(boolean[][] maze);
-    public void print(boolean[][] maze);
-}
