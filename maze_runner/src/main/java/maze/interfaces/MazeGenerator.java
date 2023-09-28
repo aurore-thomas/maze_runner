15613c7025a014061100ed87877d01cd447c90ec
aurore-thomas <aurore.thomas@laplateforme.io>
2023-09-28 15:25:45 +0200

    [UPDATE] Architecure : files put at the root of the project

--- maze_runner/src/main/java/maze/interfaces/MazeGenerator.java
+++
@@ -1,7 +0,0 @@
-package maze.interfaces;
-
-public interface MazeGenerator {
-    public void initializeMaze(int inputColumns, int inputLines);
-    public boolean[][] generateCenterMaze(int x, int y);
-    public void print(boolean[][] maze);
-}
