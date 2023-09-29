290d3dbe2b00b9396e7f5cdfce410d3c1e157cdf
aurore-thomas <aurore.thomas@laplateforme.io>
2023-09-29 17:06:32 +0200

    [UPDATE] Error input gestion and redirection to maze

--- src/main/java/MazeRunner.java
+++ src/main/java/MazeRunner.java
@@ -1,17 +1,51 @@
-import maze.generators.SimpleImperfectMazeGenerator;
+import maze.Maze;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MazeRunner {
-    public static void main(String[] args) {
-//        SimplePerfectMazeGenerator maze = new SimplePerfectMazeGenerator();
-//        maze.initializeMaze(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-//        boolean[][] finalMaze = maze.generateCenterMaze(1, 1);
-////        maze.lastLineMaze(finalMaze);
-//        maze.print(finalMaze);
+    public static int width, height;
+    public static String type, status;
 
-        SimpleImperfectMazeGenerator maze = new SimpleImperfectMazeGenerator();
-        maze.initializeMaze(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-        boolean[][] finalMaze = maze.generateCenterMaze(1, 1);
-        maze.destroyWall(finalMaze);
-        maze.print(finalMaze);
+    private static boolean getSize(String[] args) {
+        try {
+            width = Integer.parseInt(args[0]);
+            height = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.print("Erreur : Veuillez fournir une largeur et une hauteur valides supérieurs à 5");
+            return false;
+        }
+
+        if (width<5 || height<5) {
+            System.out.print("Erreur : Veuillez fournir une largeur et une hauteur valides supérieurs à 5");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean getInput(String[] args) {
+        status = args[2];
+        type = args[3];
+
+        List<String> authorizedStatus = new ArrayList<>(List.of("perfect", "imperfect"));
+        List<String> authorizedTypes = new ArrayList<>(List.of("simple", "graph", "optimized"));
+        return authorizedTypes.contains(type) && authorizedStatus.contains(status);
+    }
+
+    public static void printManual() {
+        System.out.println("Utilisation : java -jar MazeRunner.jar [largeur] [hauteur] [perfect/imperfect] [simple/graph/optimized]");
+    }
+
+    public static void main(String[] args) {
+        if (!getSize(args)) {
+            printManual();
+        } else if (!getInput(args)) {
+            System.out.println("Erreur : Veuillez fournir un type de labyrinthe et une méthode de génération valides.");
+            printManual();
+        } else {
+            System.out.println("Entrée main");
+            Maze newMaze = new Maze(width, height, status, type);
+            newMaze.choiceType();
+        }
     }
 }
